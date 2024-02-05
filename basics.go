1d75bf68f2d2330fae732f22e0c1df8b47b291de
Toby Bull <toby@tobyybull.uk>
2024-02-05 16:51:42 +0000

    A few of the basics

---
+++ basics.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func variables() {
+	var a int
+	a = 1
+	var b int = 3
+	var c = 5
+	d := 6
+
+	fmt.Printf("%d %d %d %d", a, b, c, d)
+}
+
+func constants() {
+	const n = 123456
+	// A number constant doesn't have a type until you give it one
+	// Or until you use it where a type must be inferred
+}
+
+func loops() {
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("Bruh!")
+		break
+	}
+	for n := 0; n < 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
+
+func conditionals() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("either 8 or 7 are even")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	// No  ternary!! Boo, you suck
+}
+
+func switch_function() {
+	// Switches are pretty good
+	// You can match on equality
+	// Or just have case statements that evaluate to a bool
+
+	i := 2
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	default:
+		println("Derp")
+	}
+
+	// Multiple cases comma deliniated
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// Check this out
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+}
+
+func arrays() {
+	// This array will have all zeros (default value of an int)
+	var a [5]int
+	fmt.Println("emp:", a)
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+	fmt.Println("len:", len(a))
+
+	// Declare like this
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+}
+
+func slices_n_stuf() {
+	// Like arrays, just don't set the length
+	// Use make keyword
+	s := make([]string, 3) // 3 is the capacity
+	println(s)
+
+	// Slices are kinda awful to be honest
+	s1 := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
+	s2 := s1[3:6]
+	fmt.Println(s2) //[d e f] - inclusive, exclusive
+
+	// But we can move the end of the range of s2 up, but not the start range down... weird af
+	fmt.Println(len(s2)) // 3
+	fmt.Println(cap(s2)) // 6  from the staring point of the slice in the underlying array to the end of the array (d to i)
+	s2 = s2[:6]
+	fmt.Println(s2) // [d e f g h i]
+}
+
+func main() {
+	variables()
+	slices_n_stuf()
+}
